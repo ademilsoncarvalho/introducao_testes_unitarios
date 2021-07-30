214b4ba2eda1f99caa37be5b64946596711a6156
Ademilson Carvalho <ade.carvalho@outlook.com>
2021-07-30 16:48:48 -0300

    Inclusão data providers

--- tests/Service/BuscaSaldoTest.php
+++ tests/Service/BuscaSaldoTest.php
@@ -49,18 +49,20 @@ class BuscaSaldoTest extends TestCase
         self::assertEquals($saldo, 20);
     }
 
-    public function testCarteiraComTransacaoDeSaida()
+    /**
+     * @dataProvider getValoresTestCarteira
+     */
+    public function testCarteiraComTransacaoDeSaida($valor1, $valor2, $expected)
     {
-
         $carteira = new Carteira();
 
         $transacao = new Transacao();
         $transacao->setTipo(Transacao::ENTRADA);
-        $transacao->setValor(50);
+        $transacao->setValor($valor1);
 
         $transacao2 = new Transacao();
         $transacao2->setTipo(Transacao::SAIDA);
-        $transacao2->setValor(10);
+        $transacao2->setValor($valor2);
 
         $carteira->adicionaTransacao($transacao);
         $carteira->adicionaTransacao($transacao2);
@@ -68,7 +70,16 @@ class BuscaSaldoTest extends TestCase
         $saldo = (new BuscaSaldo())->buscaSaldoCarteira($carteira);
 
         self::assertIsFloat($saldo);
-        self::assertEquals($saldo, 40);
+        self::assertEquals($expected, $saldo);
+    }
+
+    public function getValoresTestCarteira(): array
+    {
+        return [
+            'Valor Maior' => [10, 5, 5],
+            'Valor Igual' => [10, 10, 0],
+            'Valor Float' => [10.8, 5.9, 4.9],
+        ];
     }
 
 }
